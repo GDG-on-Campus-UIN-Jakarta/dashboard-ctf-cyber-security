c9f0f5e70469d7422fef4c48e3f400a5322b9ca2
oskhar <muhamadoskharm@gmail.com>
2025-01-02 02:46:22 +0700

    fix: minor component

--- user-dash/react-next-js/src/components/Providers.jsx
+++ user-dash/react-next-js/src/components/Providers.jsx
@@ -4,7 +4,6 @@ import { SettingsProvider } from '@core/contexts/settingsContext'
 import ThemeProvider from '@components/theme'
 
 // Component Imports
-import UpgradeToProButton from '@components/upgrade-to-pro-button'
 
 // Util Imports
 import { getMode, getSettingsFromCookie } from '@core/utils/serverHelpers'
@@ -22,7 +21,6 @@ const Providers = props => {
       <SettingsProvider settingsCookie={settingsCookie} mode={mode}>
         <ThemeProvider direction={direction}>
           {children}
-          <UpgradeToProButton />
         </ThemeProvider>
       </SettingsProvider>
     </VerticalNavProvider>
